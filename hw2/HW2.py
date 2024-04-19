5abe726fd2bd03eeaa623ef10fe375249736b2a4
taylorswrite <wtmartinez@ucla.edu>
2024-04-19 11:58:16 -0700

    Modified HW1 PDF. Initialized HW2.

---
+++ hw2/HW2.py
@@ -0,0 +1,23 @@
+# HW2
+# Name:
+# Collaborators:
+# Date:
+
+import random
+
+def count_characters(s):
+    """WRITE DOCSTRING HERE
+    """
+    pass # replace with your code
+
+
+def count_ngrams(s, n):
+    """WRITE DOCSTRING HERE
+    """
+    pass # replace with your code
+
+
+def markov_text(s, n, length, seed):
+    """WRITE DOCSTRING HERE
+    """
+    pass # replace with your code
