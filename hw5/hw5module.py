46f4295a4065e3a7c5a2877865ec2d77bc7011c2
taylorswrite <wtmartinez@ucla.edu>
2024-05-27 11:27:25 -0700

    added files

---
+++ hw5/hw5module.py
@@ -0,0 +1,7 @@
+def parse_actor_page(movie_df, actor_directory):
+    print(f"{type(movie_df)}\n{movie_df.head(2)}\n{type(actor_directory)}:{actor_directory}")
+
+    
+    
+def parse_full_credits(movie_directory):
+    print(f"{type(movie_directory)}:{movie_directory}")
